b15c7e1eeea0978f3665bf5439ca4c1e5ce99b73
JyothikaHoney <jyothikahoney316@gmail.com>
2023-12-23 19:02:56 +0530

    whileloop

---
+++ WhileLoop.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int i=0; //initialization
+    while(i<5) //condition
+    {
+        printf("Cprogramming\n");
+        i++; //updation
+    }
+    return 0;
+}
