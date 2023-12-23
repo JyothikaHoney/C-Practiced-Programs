f50a6a5661422f148ad01642efb7ed14e644999c
JyothikaHoney <jyothikahoney316@gmail.com>
2023-12-23 19:43:21 +0530

    second commit

---
+++ PrintName.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main()
+{
+    char name[30];
+    printf("Enter your name : ");
+    scanf("%s",&name);
+    printf("Welcome, %s\n",name);
+}
---
+++ PrintingName.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+
+void main()
+{
+    printf("Hai, welcome to programming");
+}
