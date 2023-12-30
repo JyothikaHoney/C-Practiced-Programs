b3b01fd12d352dfd8045aa3fd25e54207621bd46
jyothikaHoney <jyothikahoney316@gmail.com>
2023-12-30 22:11:12 +0530

    adding files

---
+++ GreaterThan.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int x,y;
+    x=15 , y=25;
+    if(x>y)
+    {
+      printf("x is greater than y\n");
+    }
+    else
+    {
+        printf("y is greater than x\n");
+    }
+}
