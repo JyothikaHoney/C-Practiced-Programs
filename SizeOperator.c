cc740cc053dbe247e6018748c9bf4caa788a7469
jyothikaHoney <jyothikahoney316@gmail.com>
2024-01-31 19:47:39 +0530

    adding files.

---
+++ SizeOperator.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+void main()
+{
+    int var;
+    printf("Size of int =%d\n",sizeof(int));
+    printf("Size of float =%d\n",sizeof(float));
+    printf("Size of var =%d\n",sizeof(var));
+    printf("Size of an integer constant=%d\n",sizeof(45));
+}
