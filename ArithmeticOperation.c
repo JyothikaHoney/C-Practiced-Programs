051205eda34b9058d9e27751784b6ebf54b4d933
jyothikaHoney <jyothikahoney316@gmail.com>
2024-02-01 18:33:06 +0530

    adding files.

---
+++ ArithmeticOperation.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+{
+    int a=17 , b=4;
+    printf("Sum=%d\n",a+b);
+    printf("Difference=%d\n",a-b);
+    printf("Product=%d\n",a*b);
+    printf("Quotient=%d\n",a/b);
+    printf("Remainder=%d\n",a%b);
+}
