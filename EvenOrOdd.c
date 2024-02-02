46ef5a94e5d923370b00c8a3c0aeacd0496dd6d0
jyothikaHoney <jyothikahoney316@gmail.com>
2024-02-02 21:01:08 +0530

    adding files

---
+++ EvenOrOdd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+    int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    if(num %2 == 0)
+    printf("\n The given number is even");
+    else;
+    printf("\n The given number is odd");
+
+}
