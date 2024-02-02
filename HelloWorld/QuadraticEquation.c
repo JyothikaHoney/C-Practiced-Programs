a8fb3658a6e1779ee0d81351c19c90310fb4e26e
jyothikaHoney <jyothikahoney316@gmail.com>
2024-02-02 20:19:14 +0530

    adding files

---
+++ HelloWorld/QuadraticEquation.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+    float a,b,c,d,e,f,x,y;
+    printf("Enter a,b,c, values:\n");
+    scanf("%f%f%f",&a,&b,&c);
+    d=b*b-4*a*c;
+    f=2*a;
+    x=(-b+e)/f;
+    y=(-b-e)/f;
+    printf("x=%5.2f\ty=%5.2f",x,y);
+}
