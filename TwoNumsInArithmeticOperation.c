1e171e2f2a10ac958474be0d82656f7270e14b25
jyothikaHoney <jyothikahoney316@gmail.com>
2024-02-06 21:14:20 +0530

    adding files

---
+++ TwoNumsInArithmeticOperation.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+{
+    int num1=15 , num2=4;
+    printf("Sum=%d\n",num1+num2);
+    printf("Difference=%d\n",num1-num2);
+    printf("Product=%d\n",num1*num2);
+    printf("Quotient=%d\n",num1/num2);
+    printf("Remainder=%d\n",num1%num2);
+}
