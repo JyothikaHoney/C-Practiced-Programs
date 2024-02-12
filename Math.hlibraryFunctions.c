c816da24d6d3a1fd0ea28ae08924e04407cb3962
jyothikaHoney <jyothikahoney316@gmail.com>
2024-02-12 13:45:59 +0530

    adding files

---
+++ Math.hlibraryFunctions.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+
+void main()
+{
+    printf("\n%f",ceil(3.6));
+    printf("\n%f",ceil(3.3));
+    printf("\n%f",floor(3.6));
+    printf("\n%f",floor(3.2));
+    printf("\n%f",sqrt(16));
+    printf("\n%f",sqrt(7));
+    printf("\n%f",pow(2,4));
+    printf("\n%f",pow(3,3));
+}
