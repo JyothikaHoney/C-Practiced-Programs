4df0829c487a26a42383b38573b3faf6d8faf24e
jyothikaHoney <jyothikahoney316@gmail.com>
2024-02-13 06:11:37 +0530

    adding files

---
+++ FormattedInputUsingscanf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+{
+    int age;
+    float height;
+    char inital;
+    char name[20];
+    printf("Enter your age,height,initial,and name:");
+    scanf("%d%f%c%s",&age,&height,&inital,&name);
+}
