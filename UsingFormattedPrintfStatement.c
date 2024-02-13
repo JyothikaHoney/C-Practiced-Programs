d1a28713f1b1159c3404290eafd76f0f22d6613e
jyothikaHoney <jyothikahoney316@gmail.com>
2024-02-13 06:00:39 +0530

    adding files

---
+++ UsingFormattedPrintfStatement.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+    int age=30;
+    float height=5.7;
+    char initial='B';
+    char name[]="Honey";
+    printf("Age:%d\n",age);
+    printf("Height:%f\n",height);
+    printf("Initial:%c\n",initial);
+    printf("Name:%s\n",name);
+}
